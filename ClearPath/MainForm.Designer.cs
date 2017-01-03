c5f590a1df06f4a213b140b36c18be484524080b
heavy <heavy@HEAVY-PC>
2017-01-03 22:11:35 +0100

    Add project files.

---
+++ ClearPath/MainForm.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace ClearPath
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnChoosePath = new System.Windows.Forms.Button();
+            this.lbPath = new System.Windows.Forms.Label();
+            this.btnClearNow = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnAccept = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnChoosePath
+            // 
+            this.btnChoosePath.Location = new System.Drawing.Point(12, 12);
+            this.btnChoosePath.Name = "btnChoosePath";
+            this.btnChoosePath.Size = new System.Drawing.Size(80, 23);
+            this.btnChoosePath.TabIndex = 0;
+            this.btnChoosePath.Text = "Choose Path";
+            this.btnChoosePath.UseVisualStyleBackColor = true;
+            this.btnChoosePath.Click += new System.EventHandler(this.btnChoosePath_Click);
+            // 
+            // lbPath
+            // 
+            this.lbPath.AutoSize = true;
+            this.lbPath.Location = new System.Drawing.Point(59, 47);
+            this.lbPath.Name = "lbPath";
+            this.lbPath.Size = new System.Drawing.Size(33, 13);
+            this.lbPath.TabIndex = 1;
+            this.lbPath.Text = "None";
+            // 
+            // btnClearNow
+            // 
+            this.btnClearNow.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClearNow.Location = new System.Drawing.Point(176, 140);
+            this.btnClearNow.Name = "btnClearNow";
+            this.btnClearNow.Size = new System.Drawing.Size(75, 23);
+            this.btnClearNow.TabIndex = 2;
+            this.btnClearNow.Text = "Clear Now!";
+            this.btnClearNow.UseVisualStyleBackColor = true;
+            this.btnClearNow.Click += new System.EventHandler(this.btnClearNow_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(12, 140);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // btnAccept
+            // 
+            this.btnAccept.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAccept.Location = new System.Drawing.Point(279, 140);
+            this.btnAccept.Name = "btnAccept";
+            this.btnAccept.Size = new System.Drawing.Size(58, 23);
+            this.btnAccept.TabIndex = 4;
+            this.btnAccept.Text = "Accept";
+            this.btnAccept.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AcceptButton = this.btnAccept;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(349, 175);
+            this.Controls.Add(this.btnAccept);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClearNow);
+            this.Controls.Add(this.lbPath);
+            this.Controls.Add(this.btnChoosePath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Clear Machine";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChoosePath;
+        private System.Windows.Forms.Label lbPath;
+        private System.Windows.Forms.Button btnClearNow;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnAccept;
+    }
+}
+
---
+++ ClearPath/MainForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClearPath
+{
+    public partial class MainForm : Form
+    {
+        private FolderBrowserDialog fbd;
+        string setFolder = Properties.Settings.Default.FolderPath;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            lbPath.Text = setFolder;
+        }
+
+        // Get folder's path
+        private void btnChoosePath_Click(object sender, EventArgs e)
+        {
+            fbd = new FolderBrowserDialog();
+            fbd.SelectedPath = setFolder;
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                Properties.Settings.Default.FolderPath = fbd.SelectedPath.ToString();
+                Properties.Settings.Default.Save();
+                lbPath.Text = fbd.SelectedPath.ToString();
+            }
+        }
+
+        //Clear current folder
+        private void btnClearNow_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure, that you want to delete ALL files from directory?",
+                "Attention",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Warning) == DialogResult.Yes)
+            {
+                DirectoryInfo di = new DirectoryInfo(Properties.Settings.Default.FolderPath);
+
+                foreach (FileInfo file in di.GetFiles())
+                {
+                    file.Delete();
+                }
+                foreach (DirectoryInfo dir in di.GetDirectories())
+                {
+                    dir.Delete(true);
+                }
+
+                MessageBox.Show("It's clear!.", "Yeah!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
